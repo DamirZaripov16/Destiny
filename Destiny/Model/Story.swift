56c628c468e9b34c7be0801fceb55ae60cea8afc
Damir Zaripov <zaripov.damir92@mail.ru>
2023-04-12 22:30:01 +0300

    initial commit

---
+++ Destiny/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  Destiny
+//
+//  Created by Damir Zaripov on 12.04.2023.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice2: String
+    let choice1Destination: Int
+    let choice2Destination: Int
+}
